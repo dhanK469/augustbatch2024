4b21187f27ce47fafeb06f3470c60e29f8002c98
dhanK469 <mdhbti2014+git@gmail.com>
2024-11-13 07:59:09 +0530

    Add files via upload

---
+++ Hello_Priya.java
@@ -0,0 +1,15 @@
+public class Hello_Priya {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		System.out.println("Without IDE");
+		
+		System.out.println("Appwars");
+		System.out.println("Priya Sharma");
+		System.out.println("Batch July 31");
+		
+
+	}
+
+}
